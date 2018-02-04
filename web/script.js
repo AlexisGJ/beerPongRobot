48eea4e467c994aa9bf8ac3cd9d5a92dbf68702c
kovalexis <alexis.giguere-joannette@mail.mcgill.ca>
2018-02-03 19:44:58 -0500

    Basic app UI + test touchmove

---
+++ web/script.js
@@ -0,0 +1,13 @@
+
+$(function() {
+    var obj = document.getElementById('appView');
+    obj.addEventListener('touchmove', function(event) {
+    // If there's exactly one finger inside this element
+    if (event.targetTouches.length == 1) {
+        var touch = event.targetTouches[0];
+        // Place element where the finger is
+        obj.style.left = touch.pageX + 'px';
+        obj.style.top = touch.pageY + 'px';
+    }
+    }, false);
+});
