cf4cf333e27abdd1a4c92a5f7c7070b060c67ec5
kovalexis <alexis.giguere-joannette@mail.mcgill.ca>
2018-02-03 22:00:02 -0500

    Console error

--- web/script.js
+++ web/script.js
@@ -1,9 +1,9 @@
 
-document.body.addEventListener('touchmove', function(event) {
-    console.log(event.source);
-    //if (event.source == document.body)
-    event.preventDefault();
-}, false);
+// document.body.addEventListener('touchmove', function(event) {
+//     console.log(event.source);
+//     //if (event.source == document.body)
+//     event.preventDefault();
+// }, false);
 
 window.onresize = function() {
     $(document.body).width(window.innerWidth).height(window.innerHeight);
