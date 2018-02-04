0dd54b9f2c4614d8c9f7068dd38e37bd1e6729e3
kovalexis <alexis.giguere-joannette@mail.mcgill.ca>
2018-02-03 19:56:31 -0500

    Alert touchstart test

--- web/script.js
+++ web/script.js
@@ -11,18 +11,9 @@ window.onresize = function() {
 
 $(function() {
     window.onresize();
+
+    document.body.addEventListener('touchstart', function(e){
+        alert(e.changedTouches[0].pageX);
+    }, false)
 });
 
-
-// $(function() {
-//     var obj = document.getElementById('appView');
-//     obj.addEventListener('touchmove', function(event) {
-//     // If there's exactly one finger inside this element
-//     if (event.targetTouches.length == 1) {
-//         var touch = event.targetTouches[0];
-//         // Place element where the finger is
-//         obj.style.left = touch.pageX + 'px';
-//         obj.style.top = touch.pageY + 'px';
-//     }
-//     }, false);
-// });
